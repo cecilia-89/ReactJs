1dcfdc64205a215df705dab465591eddafe742d4
Cecilia Atabong <catabong89@gmail.com>
2022-04-19 09:46:54 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/navbar';
 import Summary from './components/summary';
 import Experience from './components/experience';
 import Skills from './components/skills';
+import React from 'react';
 
 
 
